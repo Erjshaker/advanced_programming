02de7163b2a1d8b5290a3d648a99942816ab3774
Erjshaker <erjohn13@gmail.com>
2020-11-03 21:29:07 +0800

    index.php of assign_two

--- index.php
+++ index.php
@@ -1,1 +1,37 @@
-<?php echo "Hello World!";?>
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>assign_two</title>
+  </head>
+  <body>
+    <?php
+      class User
+      {
+        function __construct($name, $username, $email)
+        {
+          $this -> name = $name;
+          $this -> username = $username;
+          $this -> email = $email;
+        }
+
+        function addFriend()
+        {
+          echo $this -> name . " added a friend";
+          echo "<br>";
+        }
+
+        function postStatus()
+        {
+          echo $this -> name . " posted a status";
+          echo "<br>";
+        }
+      }
+
+      $user = new User("Erjohn Espuerta", "Erjshaker", "erjohn13@gmail.com");
+      $user -> addFriend();
+      echo "<br>";
+      $user -> postStatus();
+    ?>
+  </body>
+</html>
